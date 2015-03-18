87d8fb94ab18e4a0f954be0c11dee92ee26f14e4
Ubuntu <simon.gorial@gmail.com>
2015-03-18 20:53:09 +0000

    Migrated CF to covapp.io blobstore

---
+++ migrate_blobs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+sudo rm -r .blobs
+rm -r blobs
+rm -r blobs_import
+git checkout -- config/blobs.yml
+git checkout -- config/final.yml
+rm config/private.yml
+
+# get blobs
+bosh sync blobs
+
+mv blobs blobs_import
+
+cp config/private.tmp.yml config/private.yml
+
+# Format for private.tmp.yml:
+# ---
+# blobstore:
+#   s3:
+#     access_key_id:     ACCESS
+#     secret_access_key: PRIVATE
+
+cat > config/final.yml <<EOF
+---
+final_name: cf-covs
+blobstore:
+  provider: s3
+  options:
+    bucket_name: covapp.io-bosh-blobstore
+    access_key_id: KEY
+    secret_access_key: SECRET
+    encryption_key: covapp-cf-release
+EOF
+
+# clear blobs.yml
+cat > config/blobs.yml <<EOF
+---
+{}
+EOF
+
+# re-add blobs
+for blob in `cd blobs_import && find */*`
+do
+  dir=`echo $blob | cut -d'/' -f1`
+  bosh add blob blobs_import/$blob $dir
+done
+
+# upload blobs
+bosh upload blobs
