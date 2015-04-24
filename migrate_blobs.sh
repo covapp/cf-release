2938ae9065a845d6006fcb5da2f0eab6c74b8b07
covapp <simon.gorial@covisint.com>
2015-04-24 19:26:02 +0000

    New RnD (covisintrnd.com-cf-blobstore) blobstore

--- migrate_blobs.sh
+++ migrate_blobs.sh
@@ -5,14 +5,14 @@ rm -r blobs
 rm -r blobs_import
 git checkout -- config/blobs.yml
 git checkout -- config/final.yml
-rm config/private.yml
+#rm config/private.yml
 
 # get blobs
 bosh sync blobs
 
 mv blobs blobs_import
 
-cp config/private.tmp.yml config/private.yml
+#cp config/private.tmp.yml config/private.yml
 
 # Format for private.tmp.yml:
 # ---
@@ -22,15 +22,12 @@ cp config/private.tmp.yml config/private.yml
 #     secret_access_key: PRIVATE
 
 cat > config/final.yml <<EOF
----
-final_name: cf-covs
+final_name: cf
 blobstore:
   provider: s3
   options:
-    bucket_name: covapp.io-bosh-blobstore
-    access_key_id: KEY
-    secret_access_key: SECRET
-    encryption_key: covapp-cf-release
+    bucket_name: covisintrnd.com-cf-blobstore
+    encryption_key: covisintrnd-cf-release
 EOF
 
 # clear blobs.yml
@@ -47,4 +44,4 @@ do
 done
 
 # upload blobs
-bosh upload blobs
+#bosh upload blobs
